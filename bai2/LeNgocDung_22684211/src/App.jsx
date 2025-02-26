6c9b3e9858016629b96c6e8b3d6045f5a4f775ae
Co Phuong Hang <Dungheongunhubo>
2025-02-27 00:07:02 +0700

    fix calculator  Bai2

--- bai2/LeNgocDung_22684211/src/App.jsx
+++ bai2/LeNgocDung_22684211/src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 import bai02 from '../image/bai02.jpg';
+import './App.css';
 
 function App() {
   const [invest, setInvest] = useState(0);
   const [rate, setRate] = useState(0);
   const [goal, setGoal] = useState(0);
   const [data, setData] = useState([]);
-  var style = {
+
+  const style = {
     margin: '50px',
-  }
+  };
 
-  var style2 = {
+  const style2 = {
     margin: '30px',
-  }
-
+  };
 
   const tableStyle = {
     border: '1px solid black',
@@ -25,86 +23,86 @@ function App() {
     borderCollapse: 'collapse',
   };
 
-function OnchangeInvest(e) {
-  setInvest(e.target.value || 0);
-}
+  function OnchangeInvest(e) {
+    setInvest(Number(e.target.value) || 0);
+  }
 
-function OnChangeRate(e) {
-  setRate(e.target.value || 0)
-}
+  function OnChangeRate(e) {
+    setRate(Number(e.target.value) || 0);
+  }
 
-function OnChangeGoal(e) {
-  setGoal(e.target.value || 0)
-}
-function onclick() {
-  let currentAmount = invest;
-    const newData = []; // Array to hold yearly data
-    let year = 2025; // Starting year
+  function OnChangeGoal(e) {
+    setGoal(Number(e.target.value) || 0);
+  }
 
-    // Calculate yearly returns until the goal is reached
-    while (currentAmount < goal) {
-      const interestEarned = currentAmount * (rate / 100); // Calculate interest for the year
-      currentAmount += interestEarned; // Update total amount
-      
+  function onclick() {
+    const currentYear = 2025;
+    const newData = [];
+    let year = currentYear;
+    let currentInvest = invest;
+    const currentRate = rate / 100;
+    let endyear = currentInvest * (1 + currentRate);
+
+    while (goal > endyear) {
       newData.push({
-        year: year++,
-        currentAmount: currentAmount.toFixed(2), // Total amount after interest
-        interestEarned: interestEarned.toFixed(2), // Interest earned this year
+        year: year,
+        invest: currentInvest,
+        rate: currentRate,
+        goal: goal,
+        endYear: endyear,
       });
+      year++;
+      endyear = currentInvest * Math.pow(1 + currentRate, year - currentYear);
     }
 
-    setData(newData); // Update data state with yearly balances
-}
+    setData(newData);
+  }
 
-return (
-  <>
-    <img src={bai02} alt="Description of the image" />
-    <div className='input-container'>
-      <div>
-        <label>Input Your Invest Money:</label>
-        <input onChange={OnchangeInvest} type="text" placeholder="Enter your money" />
+  return (
+    <>
+      <img src={bai02} alt="Description of the image" />
+      <div className='input-container'>
+        <div>
+          <label>Input Your Invest Money:</label>
+          <input onChange={OnchangeInvest} type="text" placeholder="Enter your money" />
+        </div>
+
+        <div>
+          <label style={style}>Input Rate:</label>
+          <input onChange={OnChangeRate} type="text" placeholder="Input rate" />
+        </div>
+
+        <div>
+          <label style={style2}>Input Your Goal:</label>
+          <input onChange={OnChangeGoal} type="text" placeholder="Enter goal" />
+        </div>
       </div>
-
-      <div>
-        <label style={style}>Input Rate:</label>
-        <input onChange={OnChangeRate} type="text" placeholder="Input rate" />
-
-      </div>
-
-      <div>
-        <label style={style2}>Input Your Goal:</label>
-        <input onChange={OnChangeGoal} type="text" placeholder="Enter goal" />
-      </div>
-    </div>
-    <br>
-    </br>
-    <br>
-    </br>
-    <table style={tableStyle}>
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Invest</th>
-          <th>Rate</th>
-          <th>Goal</th>
-          <th>EndYear</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => (
-          <tr key={index}>
-            <td>{row.year}</td>
-            <td>{row.invest}</td>
-            <td>{row.rate}</td>
-            <td>{row.goal}</td>
-            <td>{row.endYear}</td>
+      <br />
+      <table style={tableStyle}>
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Invest</th>
+            <th>Rate</th>
+            <th>Goal</th>
+            <th>EndYear</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-    <button onClick={onclick}>Click</button>
-  </>
-)
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>{row.year}</td>
+              <td>{row.invest}</td>
+              <td>{(row.rate * 100).toFixed(2)}%</td>
+              <td>{row.goal}</td>
+              <td>{row.endYear.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={onclick}>Click</button>
+    </>
+  );
 }
 
-export default App
+export default App;
