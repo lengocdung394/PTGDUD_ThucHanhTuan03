e10f871b9555ec068e8be05a6c163770056e882c
Co Phuong Hang <Dungheongunhubo>
2025-02-28 19:02:09 +0700

    fixed css Footer, MainContent

--- bai1/LeNgocDung_22684211/component/MainContent/MainContent.jsx
+++ bai1/LeNgocDung_22684211/component/MainContent/MainContent.jsx
@@ -3,18 +3,51 @@ import './MainContent.css'
 import hinhanh from '../../image/image.png'
 
 export default function MainContent() {
+    var style1 = {
+        color: '#FF4081',
+        fontSize: '15px',
+        textAlign: 'center',
+        backgroundColor: 'rgb(244, 207, 223)',
+        padding: '10px 10px',
+        border: 'none',
+        borderRadius: '20px',
+        cursor: 'pointer',
+        marginright: '10px',
+    }
+    var style2 = {
+        color: '#FF4081',
+        fontSize: '15px',
+        textAlign: 'center',
+        backgroundColor:'rgb(245, 205, 246)',
+        padding: '10px 10px',
+        border: 'none',
+        borderRadius: '20px',
+        cursor: 'pointer',
+        marginright: '10px',
+    }
+    var style3 = {
+        color: '#9feeee',
+        fontSize: '15px',
+        textAlign: 'center',
+        backgroundColor: 'rgb(197, 243, 248)',
+        padding: '10px 10px',
+        border: 'none',
+        borderRadius: '20px',
+        cursor: 'pointer',
+        marginright: '10px',
+    }
     return (
         <div class="content">
             <div id="left"></div>
             <div id="right">
                 <h2>Sory, no results were found for "cakescascsa"</h2>
-                <img src={hinhanh} alt="" />
+                <img id="image" src={hinhanh} alt="" />
                 <p >We have all your independence Day sweets covered</p>
                 <div id="buttons">
-                    <button>Sweet Cake</button>
-                    <button>Black Cake</button>
-                    <button>Pozele Verde</button>
-                    <button>Healthy Food</button>
+                    <button style = {style1}>Sweet Cake</button>
+                    <button style = {style2}>Black Cake</button>
+                    <button style = {style1}>Pozele Verde</button>
+                    <button style = {style3}>Healthy Food</button>
                 </div>
             </div>
         </div>
